721f25452a863625d83c2f0579ec6ec8bd16304d
mikhawa <michaeljpitz@gmail.com>
2024-10-24 15:01:06 +0200

    roles ok

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Article;
+use App\Entity\Section;
 
 class AdminController extends AbstractController
 {
