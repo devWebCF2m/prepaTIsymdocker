267ec801d6b6bfb76ee56426d4dc4ff021f40a46
mikhawa <michaeljpitz@gmail.com>
2024-10-24 14:42:06 +0200

    login ok

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,36 +4,27 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-# Appel de l'ORM Doctrine
 use Doctrine\ORM\EntityManagerInterface;
-# Importation de l'entité Section
 use App\Entity\Section;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/connect', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils,EntityManagerInterface $entityManager): Response
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils,EntityManagerInterface $em): Response
     {
         if ($this->getUser()) {
-            return $this->redirectToRoute('homepage');
+             return $this->redirectToRoute('homepage');
          }
 
-        // on récupère toutes les catégories
-        $categories = $entityManager->getRepository(Section::class)->findAll();
-
+        $categories = $em->getRepository(Section::class)->findAll();
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('main/security/login.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error,
-            // on envoie les catégories à la vue
-            'categories' => $categories,
-            ]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error,'categories' => $categories]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
